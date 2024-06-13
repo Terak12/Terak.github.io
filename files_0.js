ef64c53897e7127617a5ad7f73ab3b5ea730cc11
Terak12 <tbhornik@gmail.com>
2024-06-13 16:45:34 -0700

    Update files_0.js

--- files_0.js
+++ files_0.js
@@ -1,5 +1,6 @@
-var searchData=
+var searchData =
 [
   ['main_2ec_0',['main.c',['../main_8c.html',1,'']]],
-  ['main_2eh_1',['main.h',['../main_8h.html',1,'']]]
+  ['main_2eh_1',['main.h',['../main_8h.html',1,]] ,
+  ['latex_2epdf_2',['Project Documentation (PDF)', 'latex.pdf',1]
 ];
